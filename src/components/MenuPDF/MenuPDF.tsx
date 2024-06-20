1a74946d3fc9edca3b001035072518a3266d77cf
Guido Pereira <stereopt@gmail.com>
2024-06-20 14:41:38 +0100

    feat: pdf from form

--- src/components/MenuPDF/MenuPDF.tsx
+++ src/components/MenuPDF/MenuPDF.tsx
@@ -1,11 +1,41 @@
-import { Document, Page, View, Text } from '@react-pdf/renderer';
+import { Font, Document, Page, View, Text } from '@react-pdf/renderer';
+import { useAtomValue } from 'jotai';
+
+import { menuAtom } from '@/store/menu.atom';
+
+import { styles } from './styles';
+
+Font.register({
+  family: 'Inter',
+  fonts: [
+    {
+      src: '/fonts/Inter-Regular.ttf',
+    },
+    {
+      src: '/fonts/Inter-SemiBold.ttf',
+      fontWeight: 'semibold',
+    },
+  ],
+});
 
 const MenuPDF = () => {
+  const menu = useAtomValue(menuAtom);
+
   return (
     <Document>
-      <Page size="A4" wrap>
-        <View fixed>
-          <Text>Easy-Menu</Text>
+      <Page style={styles.page} size="A4" wrap>
+        <View style={styles.header} fixed>
+          <Text style={styles.title}>{menu.title}</Text>
+          <Text style={styles.subtitle}>{menu.subtitle}</Text>
+        </View>
+        <View style={styles.itemWrapper}>
+          <View style={styles.itemWrapper2}>
+            <Text style={styles.productName}>{menu.productName}</Text>
+            <Text style={styles.productDescription}>
+              {menu.productDescription}
+            </Text>
+          </View>
+          <Text style={styles.productPrice}>{menu.productPrice} €</Text>
         </View>
       </Page>
     </Document>
--- src/store/menu.atom.ts
+++ src/store/menu.atom.ts
@@ -1,4 +1,11 @@
+import { MenuFormInputs } from '@/schemas/menuForm';
 import { atom } from 'jotai';
 
-export const menuAtom = atom({});
-export const menuCreated = atom(false);
+export const menuAtom = atom<MenuFormInputs>({
+  title: '',
+  subtitle: '',
+  productName: '',
+  productDescription: '',
+  productPrice: 0,
+});
+export const menuCreated = atom<boolean>(false);
