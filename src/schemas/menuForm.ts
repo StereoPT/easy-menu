07e8fd7783590e8eda6cb2affe8979bff02d7e01
Guido Pereira <stereopt@gmail.com>
2024-06-20 18:21:04 +0100

    refactor: form validation messages

--- src/schemas/menuForm.ts
+++ src/schemas/menuForm.ts
@@ -4,13 +4,27 @@ const TitleMessages = {
   required: 'Menu Title is required!',
 };
 
+const ProductNameMessages = {
+  required: 'Product Name is required!',
+};
+
+const ProductPriceMessages = {
+  required: 'Product Price is required!',
+  number: 'Product Price must be a Number!',
+  min: 'Product Price must be greater than 0!',
+};
+
 export const menuFormSchema = yup
   .object({
     title: yup.string().trim().required(TitleMessages.required),
     subtitle: yup.string().trim().optional(),
-    productName: yup.string().trim().required(),
+    productName: yup.string().trim().required(ProductNameMessages.required),
     productDescription: yup.string().trim().optional(),
-    productPrice: yup.number().min(0).required(),
+    productPrice: yup
+      .number()
+      .typeError(ProductPriceMessages.number)
+      .required(ProductPriceMessages.required)
+      .min(0, ProductPriceMessages.min),
   })
   .required();
 
