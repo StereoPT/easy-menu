ccc265c9016ef92976c233476648a88cbfb84287
Guido Pereira <stereopt@gmail.com>
2024-06-24 18:22:09 +0100

    feat: movable items (#7)

--- src/components/MenuBuilder/MenuItem/MenuItem.tsx
+++ src/components/MenuBuilder/MenuItem/MenuItem.tsx
@@ -1,16 +1,45 @@
 import FormInput from '@/components/FormInput/FormInput';
 import { Button } from 'react-daisyui';
-import { UseFieldArrayRemove } from 'react-hook-form';
-import { FiTrash2 } from 'react-icons/fi';
+import { UseFieldArrayMove, UseFieldArrayRemove } from 'react-hook-form';
+import { FiArrowDown, FiArrowUp, FiTrash2 } from 'react-icons/fi';
 
 type MenuItemProps = {
   itemIndex: number;
+  itemAmount: number;
   removeItem: UseFieldArrayRemove;
+  moveItem: UseFieldArrayMove;
 };
 
-const MenuItem = ({ itemIndex, removeItem }: MenuItemProps) => {
+const MenuItem = ({
+  itemIndex,
+  itemAmount,
+  removeItem,
+  moveItem,
+}: MenuItemProps) => {
   return (
     <div className="flex justify-between gap-2 w-full p-2 bg-neutral-200 rounded-xl">
+      <div className="flex flex-col gap-2">
+        {itemIndex > 0 && (
+          <Button
+            type="button"
+            size="sm"
+            shape="square"
+            color="ghost"
+            onClick={() => moveItem(itemIndex, itemIndex - 1)}>
+            <FiArrowUp size={20} />
+          </Button>
+        )}
+        {itemAmount > 1 && itemIndex + 1 < itemAmount && (
+          <Button
+            type="button"
+            size="sm"
+            shape="square"
+            color="ghost"
+            onClick={() => moveItem(itemIndex, itemIndex + 1)}>
+            <FiArrowDown size={20} />
+          </Button>
+        )}
+      </div>
       <div className="flex flex-col gap-2 basis-2/3">
         <div className="w-full">
           <FormInput
