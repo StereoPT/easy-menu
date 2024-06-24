8e8e7ac5c529b8500a49aabb549a3d92acc2cefe
Guido Pereira <stereopt@gmail.com>
2024-06-24 12:13:58 +0100

    feat: download PDF modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,13 @@
-import dynamic from 'next/dynamic';
-
-import { Button } from 'react-daisyui';
 import MenuBuilder from '@/components/MenuBuilder/MenuBuilder';
-import MenuPDF from '@/components/MenuPDF/MenuPDF';
 
-import { useAtomValue } from 'jotai';
-
-import { menuCreated } from '@/store/menu.atom';
 import Navbar from '@/components/Navbar/Navbar';
 
-const PDFDownloadLink = dynamic(
-  () => import('@react-pdf/renderer').then((mod) => mod.PDFDownloadLink),
-  {
-    ssr: false,
-  },
-);
-
 const Home = () => {
-  const isMenuCreated = useAtomValue(menuCreated);
-
   return (
     <>
       <Navbar />
       <main className="flex flex-col items-center gap-16 p-4 mt-12">
         <MenuBuilder />
-        <div className="flex justify-center">
-          {isMenuCreated && (
-            <div>
-              <PDFDownloadLink document={<MenuPDF />} fileName="easy-menu.pdf">
-                {({ blob, url, loading, error }) => (
-                  <Button color="primary" loading={loading} disabled={loading}>
-                    Download Now
-                  </Button>
-                )}
-              </PDFDownloadLink>
-            </div>
-          )}
-        </div>
       </main>
     </>
   );
