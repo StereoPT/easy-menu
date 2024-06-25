83d03f2d6e83e803d0a05e87d8e70cea08a66cd0
Guido Pereira <stereopt@gmail.com>
2024-06-25 14:26:43 +0100

    fix: disable instead of hide

--- src/components/MenuBuilder/MenuItem/MenuItem.tsx
+++ src/components/MenuBuilder/MenuItem/MenuItem.tsx
@@ -19,26 +19,24 @@ const MenuItem = ({
   return (
     <div className="flex justify-between gap-2 w-full p-2 bg-neutral-200 rounded-xl">
       <div className="flex flex-col gap-2">
-        {itemIndex > 0 && (
-          <Button
-            type="button"
-            size="sm"
-            shape="square"
-            color="ghost"
-            onClick={() => moveItem(itemIndex, itemIndex - 1)}>
-            <FiArrowUp size={20} />
-          </Button>
-        )}
-        {itemAmount > 1 && itemIndex + 1 < itemAmount && (
-          <Button
-            type="button"
-            size="sm"
-            shape="square"
-            color="ghost"
-            onClick={() => moveItem(itemIndex, itemIndex + 1)}>
-            <FiArrowDown size={20} />
-          </Button>
-        )}
+        <Button
+          type="button"
+          size="sm"
+          shape="square"
+          color="ghost"
+          disabled={itemIndex <= 0}
+          onClick={() => moveItem(itemIndex, itemIndex - 1)}>
+          <FiArrowUp size={20} />
+        </Button>
+        <Button
+          type="button"
+          size="sm"
+          shape="square"
+          color="ghost"
+          disabled={itemIndex + 1 >= itemAmount}
+          onClick={() => moveItem(itemIndex, itemIndex + 1)}>
+          <FiArrowDown size={20} />
+        </Button>
       </div>
       <div className="flex flex-col gap-2 basis-2/3">
         <div className="w-full">
@@ -68,6 +66,7 @@ const MenuItem = ({
         size="sm"
         shape="square"
         color="error"
+        disabled={itemAmount <= 1}
         onClick={() => removeItem(itemIndex)}>
         <FiTrash2 size={20} />
       </Button>
