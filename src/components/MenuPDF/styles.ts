9ed22ece7b3d21da589e912f1d0cf1a64a05315a
Guido Pereira <stereopt@gmail.com>
2024-07-02 10:06:06 +0100

    feat: pdf design (#11)
    * chore: menu types
    
    * feat: menu design

--- src/components/MenuPDF/styles.ts
+++ src/components/MenuPDF/styles.ts
@@ -1,68 +1,93 @@
 import { Styles, StyleSheet } from '@react-pdf/renderer';
 
 const customCss: Styles = {
+  // ***** PDF Page ***** //
   page: {
-    paddingVertical: '32px',
-    paddingHorizontal: '32px',
+    fontFamily: 'Inter',
   },
+
+  // ***** PDF Header ***** //
   header: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    gap: '8px',
+
+    backgroundColor: '#e5e5e5',
+
+    paddingHorizontal: '32px',
+    paddingVertical: '32px',
     marginBottom: '32px',
   },
   title: {
-    fontSize: '24px',
-    fontFamily: 'Inter',
+    fontSize: '32px',
     fontWeight: 'semibold',
+    letterSpacing: '4px',
+    textTransform: 'uppercase',
     color: '#55cc8a',
   },
   subtitle: {
-    fontSize: '14px',
-    fontFamily: 'Inter',
+    fontSize: '20px',
     color: '#333c4d',
   },
-  categories: {
+
+  // ***** PDF Content ***** //
+  content: {
     display: 'flex',
     flexDirection: 'column',
     gap: '24px',
+
+    paddingHorizontal: '32px',
     marginHorizontal: '32px',
   },
+
+  // ***** PDF Category ***** //
   category: {
     display: 'flex',
     flexDirection: 'column',
     gap: '16px',
   },
+  categoryName: {
+    display: 'flex',
+    justifyContent: 'center',
+
+    textAlign: 'center',
+    paddingHorizontal: '16px',
+    paddingVertical: '4px',
+
+    borderRadius: '4px',
+
+    backgroundColor: '#66cc8a',
+  },
+
+  // ***** PDF Item ***** //
   items: {
     display: 'flex',
     flexDirection: 'column',
     gap: '16px',
   },
-  itemWrapper: {
+  item: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: '32px',
   },
-  itemWrapper2: {
+  itemWrapper: {
     display: 'flex',
     flexDirection: 'column',
   },
   itemName: {
     fontSize: '16px',
-    fontFamily: 'Inter',
     fontWeight: 'semibold',
     color: '#333c4d',
   },
   itemDescription: {
     fontSize: '12px',
-    fontFamily: 'Inter',
-    color: '#333c4d',
+    color: '#737373',
   },
   itemPrice: {
     fontSize: '16px',
-    fontFamily: 'Inter',
     color: '#333c4d',
   },
 };
