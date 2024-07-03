56035cb2d235673264d5ba0df67500d0aafcd312
Guido Pereira <stereopt@gmail.com>
2024-07-03 17:58:09 +0100

    feat: SEO (#12)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,66 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
     <>
       <Head>
         <title>Easy-Menu</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta
+          name="description"
+          content="Easy Menu is a cutting-edge web application that empowers businesses to create stunning, professional-grade PDF pricing lists with unparalleled ease."
+        />
+        <meta
+          name="keywords"
+          content="Customizable PDF pricing lists,
+Professional pricing list generator,
+Restaurant menu design,
+Cafe menu creator,
+Digital pricing lists,
+Printable pricing lists,
+Business pricing list tool,
+Easy menu creation,
+High-quality menu generator,
+User-friendly menu design,
+Custom PDF menu maker,
+Online pricing list app,
+Restaurant menu PDF,
+Pricing list software,
+Menu design application,
+Digital menu creation,
+PDF menu customization,
+Business menu solution,
+PDF menu generator,
+Menu design software"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+
+        <meta property="og:site_name" content="Easy-Menu" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content="Easy-Menu" />
+        <meta
+          property="og:description"
+          content="Easy Menu is a cutting-edge web application that empowers businesses to create stunning, professional-grade PDF pricing lists with unparalleled ease."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://easy-menu-one.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://easy-menu-one.vercel.app/images/thumbnail.png"
+        />
+        <meta property="og:image:alt" content="Easy-Menu" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Easy-Menu" />
+        <meta
+          name="twitter:description"
+          content="Easy Menu is a cutting-edge web application that empowers businesses to create stunning, professional-grade PDF pricing lists with unparalleled ease.."
+        />
+        <meta
+          name="twitter:image"
+          content="https://easy-menu-one.vercel.app/images/thumbnail.png"
+        />
       </Head>
       <Theme dataTheme="emerald">
         {getLayout(<Component {...pageProps} />)}
