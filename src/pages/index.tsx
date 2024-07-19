187f0b4feee48844ae38914122c4a88ebc120eff
Guido Pereira <stereopt@gmail.com>
2024-07-19 11:50:18 +0100

    feat: footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,20 @@ import Navbar from '@/components/Navbar/Navbar';
 import Hero from '@/components/LandingPage/Hero';
 import FeatureList from '@/components/LandingPage/FeatureList';
 import EmailSignup from '@/components/LandingPage/EmailSignup';
+import Footer from '@/components/Footer/Footer';
 
 const Home = () => {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <main className="flex flex-col items-center overflow-y-auto overflow-x-hidden gap-24 py-8">
-        <Hero />
-        <FeatureList />
-        <EmailSignup />
-      </main>
+      <div className="flex flex-col items-center overflow-y-auto overflow-x-hidden gap-24">
+        <main className="flex flex-col items-center gap-24 w-full pt-8">
+          <Hero />
+          <FeatureList />
+          <EmailSignup />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
