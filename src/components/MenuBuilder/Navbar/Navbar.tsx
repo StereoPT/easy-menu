48245c0fe426e53b88a449df33db7c47de9f5e59
Guido Pereira <stereopt@gmail.com>
2024-08-05 15:05:33 +0100

    feat: builder logo

--- src/components/MenuBuilder/Navbar/Navbar.tsx
+++ src/components/MenuBuilder/Navbar/Navbar.tsx
@@ -5,6 +5,8 @@ import { useSetAtom } from 'jotai';
 import { menuAtom, previewAtom } from '@/store/menu.atom';
 import { SubmitHandler, useFormContext } from 'react-hook-form';
 import { MenuFormInputs } from '@/schemas/menuForm';
+import Image from 'next/image';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Navbar = () => {
   const setMenuAtom = useSetAtom(menuAtom);
@@ -19,11 +21,25 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 right-0 left-0 flex items-center justify-between bg-white px-3 py-2 z-50">
-      <Link href="/">
-        <Button color="ghost" size="sm">
-          Easy-Menu
-        </Button>
-      </Link>
+      <div className="flex flex-row items-center">
+        <Link href="/">
+          <Button color="ghost" size="sm">
+            <Image
+              src="/images/logoBW.png"
+              alt="Website Logo"
+              width={24}
+              height={24}
+            />
+          </Button>
+        </Link>
+        <FiChevronRight />
+        <Link href="/">
+          <Button color="ghost" size="sm">
+            Easy-Menu
+          </Button>
+        </Link>
+      </div>
+
       <div className="flex items-center gap-2">
         <Button
           type="button"
